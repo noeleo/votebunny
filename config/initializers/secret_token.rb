9f35bcfc1051db577576ae37daa38023f4634757
Noel Moldvai <noelmoldvai@gmail.com>
2012-11-27 02:01:34 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votebunny::Application.config.secret_token = 'df829cb5713e7e8bc46ffe283b4ea63cadf4f58e4d65caad695fcd35d9ec54a463d76462f71e3f204893ee60d24b523e215a9020dd0a2d585af3c0bdea1d5fdf'
